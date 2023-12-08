363e292ceaefbf040c148d96c42af8be30bf8320
Siddarth1729 <sidgarik9@gmail.com>
2023-12-08 23:03:02 +0530

    technohacks

---
+++ task2/calculator.js
@@ -0,0 +1,31 @@
+let displayValue = "0";
+
+function updateDisplay() {
+    document.getElementById("display").textContent = displayValue;
+}
+
+function appendToDisplay(value) {
+    if (displayValue === "0" && value !== '.') {
+        displayValue = value;
+    } else {
+        displayValue += value;
+    }
+    updateDisplay();
+}
+
+function clearDisplay() {
+    displayValue = "0";
+    updateDisplay();
+}
+
+function calculateResult() {
+    try {
+        displayValue = eval(displayValue).toString();
+        if (displayValue === "Infinity" || displayValue === "-Infinity") {
+            displayValue = "Error";
+        }
+    } catch (error) {
+        displayValue = "Error";
+    }
+    updateDisplay();
+}
